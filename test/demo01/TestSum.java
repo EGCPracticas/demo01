3e7dcd66dea4ac1ed4ed22894aa6e3edcf4bc63b
JoinHearthstone <joinhearthstone@outlook.es>
2016-10-20 10:55:22 +0200

    Support for zero added

--- test/demo01/TestSum.java
+++ test/demo01/TestSum.java
@@ -10,9 +10,16 @@ public class TestSum {
 	public void testSumTwoNumbers() {
 		assertEquals(4, sum(3,1));
 	}
+	
+	@Test
+	public void testSumANumberAndZero() {
+		assertEquals(6, sum(6,0));
+		assertEquals(6, sum(0,6));
+
+	}
 
 	private int sum(int i, int j) {
-		return 4;
+		return i+j;
 	}
 
 }
