a3cdb5b437d354022ace0277b83a8440f21d7202
JoinHearthstone <joinhearthstone@outlook.es>
2016-10-28 09:56:02 +0200

    Primera prueba

---
+++ test/demo01/TestRest.java
@@ -0,0 +1,19 @@
+package demo01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRest {
+
+	@Test
+	public void restaDosNumeros() {
+		assertEquals(2, resta(5, 3));
+	}
+
+	private int resta(int i, int j) {
+		
+		return 2;
+	}
+
+}
